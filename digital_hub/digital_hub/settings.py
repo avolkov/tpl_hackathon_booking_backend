ac485a1cbd93eb753c0f82f2535af28fec6d1e2d
Alex Volkov <alex@flamy.ca>
2016-09-18 14:24:40 -0400

    Using sqlite as default config.

--- digital_hub/digital_hub/settings.py
+++ digital_hub/digital_hub/settings.py
@@ -78,16 +78,19 @@ WSGI_APPLICATION = 'digital_hub.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'digital_hub',
-        'USER': 'alex',
-        'PASSWORD': 'alex',
-        'HOST': 'localhost',
-        'PORT': '5433'
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+# 'default': {
+#    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#    'NAME': 'digital_hub',
+#    'USER': 'alex',
+#    'PASSWORD': 'alex',
+#    'HOST': 'localhost',
+#    'PORT': '5433'
+#    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
