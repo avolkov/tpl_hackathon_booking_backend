4708a179618459ecd56099f406848c9e394cbd71
Alex Volkov <alex@flamy.ca>
2016-09-18 15:26:02 -0400

    Got static assets working.

--- digital_hub/digital_hub/settings.py
+++ digital_hub/digital_hub/settings.py
@@ -114,11 +114,8 @@ AUTH_PASSWORD_VALIDATORS = [{
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'America/Toronto'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
--- digital_hub/digital_hub/urls.py
+++ digital_hub/digital_hub/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = 'Digital Hub Resource Manager'
 admin.site.site_title = "Digital Hub Resource Manager"
 urlpatterns = [
     url(r'^library_branch/', include('library_branch.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
